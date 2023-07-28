e0712a287e3e5b20831a57f5373f0df950f9e74c
DEHucka <d.matytsin@white-soft.ru>
2023-07-28 13:43:53 +1000

    почищено всё

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.22"
-    `java-library`
-    `maven-publish`
-//    signing
 }
 
 repositories {
@@ -12,46 +9,4 @@ repositories {
 }
 
 dependencies {
-}
-
-allprojects {
-    group = "com.dehucka"
-    version = "0.1-SNAPSHOT"
-}
-
-subprojects {
-    apply(plugin = "java-library")
-    apply(plugin = "maven-publish")
-//    apply(plugin = "signing")
-
-    publishing {
-        publications {
-            create<MavenPublication>(project.name) {
-                groupId = project.group as String
-                artifactId = project.name
-                version = project.version as String
-
-                from(components["java"])
-            }
-        }
-    }
-
-    java {
-        withJavadocJar()
-        withSourcesJar()
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    tasks.test {
-        useJUnitPlatform()
-    }
-
-    tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-
-    sourceSets.main {
-        java.srcDirs("build/generated/ksp/main/kotlin")
-    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,2 @@
 
-rootProject.name = "micro-service-extensions"
-include("eureka-extensions")
-include("telegram-bot")
-include("micro-service-core")
-include("exposed-extensions")
+rootProject.name = "microservice-extensions"
